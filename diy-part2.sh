d6d506d609974177a64c01b61a8e979c828d9a4c
luohao10001 <55565872+luohao10001@users.noreply.github.com>
2021-08-21 16:24:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,8 +26,8 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/Boos4721/OpenWrt-Packages.git package/luci-app-eqos
-git clone https://github.com/Boos4721/OpenWrt-Packages.git package/luci-app-advanced
+git clone https://github.com/Boos4721/OpenWrt-Packages.git package/eqos
+git clone https://github.com/Boos4721/OpenWrt-Packages.git package/advanced
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
