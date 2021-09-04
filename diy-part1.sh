38b35d3fac5ff6633a15b124c007c45e7cf4b0ed
luohao10001 <55565872+luohao10001@users.noreply.github.com>
2021-09-04 23:38:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,10 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#修改wifi
+sed -i 's/wireless.default_radio2{devidx}.ssid= Xiaomi_2.4G/wireless.default_radio2{devidx}.ssid=luohao/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.default_radio1{devidx}.ssid= Xiaomi_5G/wireless.default_radio1{devidx}.ssid=luohao_5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.default_radio${devidx}.key= 1234567890/wireless.default_radio${devidx}.key=1990luohao/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
